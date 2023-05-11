02bf7b919c1e6a850aeb6b7543679d3304496475
Basek Kelziye <bowaelal7er@gmail.com>
2023-05-11 17:50:45 +0200

    last changes

--- routes/blog.js
+++ routes/blog.js
@@ -1,47 +1,63 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
-const db = require("../data/database");
+const db = require('../data/database');
 
 var loggedIn = false;
 
-router.get("/", function (req, res) {
-  res.redirect("/posts");
+router.get('/', function (req, res) {
+  res.redirect('/login');
 });
 
-router.get("/login", function (req, res) {
-  res.render("login");
+router.get('/login', function (req, res) {
+  res.render('login');
 });
-router.post("/login", async function (req, res) {
-  console.log("name " + req.body.name + "pw " + req.body.password);
+router.post('/login', async function (req, res) {
+  console.log('name ' + req.body.name + 'pw ' + req.body.password);
+  const userName = req.body.name;
+  const userPassword = req.body.password;
+
+  console.log('user sessoin is :');
+  console.log(req.session);
+
+  req.session.user = {
+    id: userName,
+    password: userPassword,
+  };
+
   const query = `SELECT * FROM authors WHERE name = '${req.body.name}' AND password = '${req.body.password}'`;
   const result = await db.query(query);
-  if (result[0].length > 0) {
-    res.redirect("/posts");
-  } else {
-    res.redirect("/login");
-  }
+  console.log(result);
+
+  req.session.isAuthenticated = true;
+  req.session.save(() => {
+    if (result[0].length > 0) {
+      res.redirect('/posts');
+    } else {
+      res.redirect('/login');
+    }
+  });
 });
 
-router.get("/new-post", async function (req, res) {
-  const [authors] = await db.query("SELECT * FROM authors");
-  console.log("in authors");
+router.get('/new-post', async function (req, res) {
+  const [authors] = await db.query('SELECT * FROM authors');
+  console.log('in authors');
   console.log(authors);
-  res.render("create-post", { authors: authors });
+  res.render('create-post', { authors: authors });
 });
 
-router.get("/posts", async function (req, res) {
+router.get('/posts', async function (req, res) {
   const query = `
      SELECT posts.*, authors.name AS author_name from posts
      INNER JOIN authors on posts.author_id = authors.id
     `;
   const [posts] = await db.query(query);
 
-  res.render("posts-list", { posts: posts });
+  res.render('posts-list', { posts: posts });
 });
 
-router.get("/posts/:id", async function (req, res) {
+router.get('/posts/:id', async function (req, res) {
   const query = `
     SELECT posts.*,authors.name AS author_name, authors.email AS author_email FROM posts
     INNER JOIN authors on posts.author_id = authors.id
@@ -50,22 +66,22 @@ router.get("/posts/:id", async function (req, res) {
 
   const [posts] = await db.query(query, [req.params.id]);
   if (!posts || posts.length === 0) {
-    return res.status(404).render("404");
+    return res.status(404).render('404');
   }
 
   const postData = {
     ...posts[0],
     date: posts[0].date.toISOString(),
-    humanReadableDate: posts[0].date.toLocaleDateString("en-US", {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
+    humanReadableDate: posts[0].date.toLocaleDateString('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
     }),
   };
-  res.render("post-detail", { post: postData });
+  res.render('post-detail', { post: postData });
 });
-router.post("/posts", async function (req, res) {
+router.post('/posts', async function (req, res) {
   const data = [
     req.body.title,
     req.body.summary,
@@ -73,13 +89,13 @@ router.post("/posts", async function (req, res) {
     req.body.author,
   ];
   await db.query(
-    "INSERT INTO posts (title, summary,body,author_id) VALUES (?)",
+    'INSERT INTO posts (title, summary,body,author_id) VALUES (?)',
     [data]
   );
-  res.redirect("/posts");
+  res.redirect('/posts');
 });
 
-router.post("/posts/:id/edit", async function (req, res) {
+router.post('/posts/:id/edit', async function (req, res) {
   const data = [
     req.body.title,
     req.body.summary,
@@ -94,25 +110,25 @@ router.post("/posts/:id/edit", async function (req, res) {
     req.body.content,
     req.params.id,
   ]);
-  res.redirect("/posts");
+  res.redirect('/posts');
 });
 
-router.get("/posts/:id/edit", async function (req, res) {
+router.get('/posts/:id/edit', async function (req, res) {
   const query = `
   SELECT * FROM posts WHERE id = ?
   `;
   const [posts] = await db.query(query, [req.params.id]);
   if (!posts || posts.length === 0) {
-    return res.status(404).render("404");
+    return res.status(404).render('404');
   }
-  res.render("update-post", { post: posts[0] });
+  res.render('update-post', { post: posts[0] });
 });
 
-router.post("/posts/:id/delete", async function (req, res) {
+router.post('/posts/:id/delete', async function (req, res) {
   const query = `
   DELETE from posts where id = ?;
   `;
   await db.query(query, [req.params.id]);
-  res.redirect("/posts");
+  res.redirect('/posts');
 });
 module.exports = router;
